6edf3c7486ae307efc1c821c96e97884fe9ed450
Mikolaj Lukasik <mlukasik@gmail.com>
2020-06-09 22:07:25 +0100

    started work on rest of interface
    rebuild the youtube view controller into swiftui view

--- Watch-Next-YouTube/YouTubeInterface.swift
+++ Watch-Next-YouTube/YouTubeInterface.swift
@@ -10,31 +10,68 @@ import SwiftUI
 
 struct YouTubeInterface: View {
     @EnvironmentObject var playerState: WatchNextPlayerState
+    @State var showInterface: Bool = true
+    @State var sliderValue: Double = 0
+    
+    func formatDuration(from seconds: Double) -> String {
+        let formatter = DateComponentsFormatter()
+        formatter.allowedUnits = [.minute, .second]
+        if seconds >= 3600 {
+            formatter.allowedUnits.insert(.hour)
+        }
+        formatter.zeroFormattingBehavior = .pad
+        formatter.unitsStyle = .positional
+
+        return formatter.string(from: TimeInterval(seconds))!
+    }
     
     var body: some View {
         ZStack {
+//            YTView()
+            
             YTView(playerState: self.playerState)
-            HStack(spacing: 0){
-                Rectangle().foregroundColor(Color("button_transparent"))
-                .onTapGesture(count: 2) {
-                    self.playerState.backwardDoubleTapped()
+            
+            VStack {
+                HStack(spacing: 0){
+                    Rectangle().foregroundColor(Color("button_transparent"))
+                    .onTapGesture(count: 2) {
+                        self.playerState.backwardDoubleTapped()
+                    }
+                    Rectangle().foregroundColor(Color("button_transparent"))
+                    .onTapGesture {
+                        self.playerState.playPauseButtonTapped()
+                    }
+                    Rectangle().foregroundColor(Color("button_transparent"))
+                    .onTapGesture(count: 2) {
+                        self.playerState.forwardDoubleTapped()
+                    }
                 }
-                Rectangle().foregroundColor(Color("button_transparent"))
-                .onTapGesture {
-                    self.playerState.playPauseButtonTapped()
-                }
-                Rectangle().foregroundColor(Color("button_transparent"))
-                .onTapGesture(count: 2) {
-                    self.playerState.forwardDoubleTapped()
+                if showInterface {
+                    HStack{
+                        Image(systemName: self.playerState.videoState == .pause || self.playerState.videoState == .stop ? "play.circle.fill" : "pause.circle.fill")
+                            .resizable()
+                            .aspectRatio(1, contentMode: .fit)
+                            .frame(width: 30, height: 30)
+                            .padding()
+                        .onTapGesture {
+                            self.playerState.playPauseButtonTapped()
+                        }
+                        Text(formatDuration(from: playerState.currentTime))
+                        Slider(value: $playerState.currentTime, in: 0...playerState.videoDuration)
+                        Text(formatDuration(from: playerState.videoDuration))
+                    }
+                    .frame(height: 50)
+                } else {
+                    /*@START_MENU_TOKEN@*/EmptyView()/*@END_MENU_TOKEN@*/
                 }
             }
         }
     }
 }
 
-struct YouTubeInterface_Previews: PreviewProvider {
-    static var previews: some View {
-        YouTubeInterface().environmentObject(WatchNextPlayerState())
-            .previewLayout(.fixed(width: 600, height: 400))
-    }
-}
+//struct YouTubeInterface_Previews: PreviewProvider {
+//    static var previews: some View {
+//        YouTubeInterface().environmentObject(WatchNextPlayerState())
+//            .previewLayout(.fixed(width: 600, height: 400))
+//    }
+//}
